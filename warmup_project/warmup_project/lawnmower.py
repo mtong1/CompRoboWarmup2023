517d25049eb703b42225d388b329d655e7659717
An Grocki <angrocki02@gmail.com>
2023-12-03 20:24:47 -0500

    update lawnmower

--- warmup_project/warmup_project/lawnmower.py
+++ warmup_project/warmup_project/lawnmower.py
@@ -1,9 +1,9 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from math import pi
-import time
-
+from std_msgs.msg import Header
+from odom_helper import TFHelper
+import time 
 
 class Lawnmower(Node):
     def __init__(self):
@@ -25,17 +25,37 @@ class Lawnmower(Node):
         self.linear_speed = 0.6 # CHANGE VALUE 
         ## orienting robot in direction of most weighted/closest objects 
         timer_period = 1
+        self.map_frame = "map"          # the name of the map coordinate frame
+        self.odom_frame = "odom"        # the name of the odometry coordinate frame
+        self.transform_helper = TFHelper(self)
+        self.threshold = .1
         self.timer = self.create_timer(timer_period, self.run_loop)
 
 
 
-    def move_straight(self):
+    def move_straight(self, x, y):
         '''
-        Moves robot based on "prey" state behavior-- it moves away from any object in front of it.
+        Moves robot straight until certain location (x,y)
         '''
-        print(self.state)
-        self.move.linear.x = self.linear_speed
-        self.publisher.publish(self.move)
+        x_difference =  abs(x-self.current_x)
+        y_difference =  abs(y - self.current_y)
+        if x_difference > self.threshold and y_difference > self.threshold:
+            self.move.linear.x = self.linear_speed
+            self.publisher.publish(self.move)
+            self.state = "move_straight"
+        else: 
+            self.state = "turn"
+    def turn(self): 
+        '''
+        Switch state to turn left or right 
+        '''
+        side = abs(self.max_x -self.current_x)
+        if side < self.threshold: 
+            #Close to max x need to turn left 
+            self.state = "turn_left"
+        else: 
+            #Close to min x, need to turn right 
+            self.state = "turn_right"
 
     def move_up_right(self):
         '''
@@ -46,6 +66,12 @@ class Lawnmower(Node):
         self.publisher.publish(self.move)
     def move_up_left(self):
         self.publisher.publish(self.move)
+    def check_object(self):
+        '''
+        Check if the object is in the current area 
+        '''
+        if self.x_object < self.threshold and self.y_object < self.threshold: 
+            self.state = "find_object"
     def find_object(): 
         """
         Publish message to main brain 
@@ -60,17 +86,21 @@ class Lawnmower(Node):
         Switches state from predator to prey when the robot "tags" something/someone.
         '''
         #logic for turning
-        if self.current_x  == self.init_x and self.current_y > self.init_y: 
-            self.state = "move_up_right"
-        elif self.current_x == self.max_x: 
-            self.state = "move_up_left"
 
+        (new_pose, delta_t) = self.transform_helper.get_matching_odom_pose(self.odom_frame,
+                                                                           self.base_frame,
+                                                                           time.localtime)
+        self.current_x = new_pose.position.x
+        self.current_y = new_pose.position.y
+        self.check_object()
         match self.state:
             case "move_straight":
                 self.move_straight()
-            case "move_up_right":
+            case "turn":
+                self.turn()
+            case "turn_right":
                 self.move_up_right()
-            case "move_up_left":
+            case "turn_left":
                 self.move_up_left
             case "find_object": 
                 self.find_object
